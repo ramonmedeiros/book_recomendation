7fd11c10f900f5ecb7ba2524581ef20d349ab796
Ramon Medeiros <ramon.medeiros@voiapp.io>
2021-02-04 13:49:20 +0100

    Adding data and first broken script

---
+++ hello.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+
+books_filename = 'BX-Books.csv'
+ratings_filename = 'BX-Book-Ratings.csv'
+
+# import csv data into dataframes
+df_books = pd.read_csv(
+    books_filename,
+    encoding = "ISO-8859-1",
+    sep=";",
+    header=0,
+    names=['isbn', 'title', 'author'],
+    usecols=['isbn', 'title', 'author'],
+    dtype={'isbn': 'str', 'title': 'str', 'author': 'str'})
+
+df_ratings = pd.read_csv(
+    ratings_filename,
+    encoding = "ISO-8859-1",
+    sep=";",
+    header=0,
+    names=['user', 'isbn', 'rating'],
+    usecols=['user', 'isbn', 'rating'],
+    dtype={'user': 'int32', 'isbn': 'str', 'rating': 'float32'})
+
+# function to return recommended books - this will be tested
+def get_recommends(book = ""):
+    X = np.array(df_books['title'])
+    nbrs = NearestNeighbors(n_neighbors=5, algorithm='ball_tree').fit(X)
+    distances, indices = nbrs.kneighbors(X)
+    return distances[5]
+
+books = get_recommends("Where the Heart Is (Oprah's Book Club (Paperback))")
+print(books)
+
+def test_book_recommendation():
+  test_pass = True
+  recommends = get_recommends("Where the Heart Is (Oprah's Book Club (Paperback))")
+  if recommends[0] != "Where the Heart Is (Oprah's Book Club (Paperback))":
+    test_pass = False
+  recommended_books = ["I'll Be Seeing You", 'The Weight of Water', 'The Surgeon', 'I Know This Much Is True']
+  recommended_books_dist = [0.8, 0.77, 0.77, 0.77]
+  for i in range(2):
+    if recommends[1][i][0] not in recommended_books:
+      test_pass = False
+    if abs(recommends[1][i][1] - recommended_books_dist[i]) >= 0.05:
+      test_pass = False
+  if test_pass:
+    print("You passed the challenge! 🎉🎉🎉🎉🎉")
+  else:
+    print("You havn't passed yet. Keep trying!")
+
+test_book_recommendation()
