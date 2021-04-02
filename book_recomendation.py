bdcd01e31c6a4b4665bb3e351d0338c47a1eb7ca
Ramon Medeiros <ramon.medeiros@voiapp.io>
2021-04-03 01:45:46 +0200

    Add comments

--- book_recomendation.py
+++ book_recomendation.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import numpy as np
 import pandas as pd
 from scipy.sparse import csr_matrix
@@ -28,9 +30,13 @@ df_ratings = pd.read_csv(
     usecols=['userId', 'movieId', 'rating'],
     dtype={'userId': 'int32', 'movieId': 'str', 'rating': 'float32'})
 
+# merge rating and catalog by movieID
 df = pd.merge(df_ratings,df_books,on='movieId')
 
+# drop title
 combine_movie_rating = df.dropna(axis = 0, subset = ['title'])
+
+# create totalRatingCount
 movie_ratingCount = (combine_movie_rating.
      groupby(by = ['title'])['rating'].
      count().
@@ -38,14 +44,15 @@ movie_ratingCount = (combine_movie_rating.
      rename(columns = {'rating': 'totalRatingCount'})
      [['title', 'totalRatingCount']]
     )
-
 rating_with_totalRatingCount = combine_movie_rating.merge(movie_ratingCount, left_on = 'title', right_on = 'title', how = 'left')
 
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
+# filter movies with more than 50 reviews
 popularity_threshold = 50
 rating_popular_movie= rating_with_totalRatingCount.query('totalRatingCount >= @popularity_threshold')
 
+# pivot table and create matrix
 movie_features_df=rating_popular_movie.pivot_table(index='title',columns='userId',values='rating').fillna(0)
 movie_features_df_matrix = csr_matrix(movie_features_df.values)
 
