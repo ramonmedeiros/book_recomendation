6d6afe35b8dc130746fa3fec8ec7507065ebf15c
Ramon Medeiros <ramon.medeiros@voiapp.io>
2021-04-04 16:27:19 +0200

    Almost there

--- book_recomendation.py
+++ book_recomendation.py
@@ -10,68 +10,80 @@ from scipy.sparse import csr_matrix
 # get data files
 books_filename = 'BX-Books.csv'
 ratings_filename = 'BX-Book-Ratings.csv'
+users_filename = "BX-Users.csv"
 
-# import csv data into dataframes
 df_books = pd.read_csv(
     books_filename,
     encoding = "ISO-8859-1",
     sep=";",
     header=0,
-    names=['movieId', 'title', 'author'],
-    usecols=['movieId', 'title', 'author'],
-    dtype={'movieId': 'str', 'title': 'str', 'author': 'str'})
+    names=['bookId', 'title', 'author'],
+    usecols=['bookId', 'title', 'author'],
+    dtype={'bookId': 'str', 'title': 'str', 'author': 'str'})
 
 df_ratings = pd.read_csv(
     ratings_filename,
     encoding = "ISO-8859-1",
     sep=";",
     header=0,
-    names=['userId', 'movieId', 'rating'],
-    usecols=['userId', 'movieId', 'rating'],
-    dtype={'userId': 'int32', 'movieId': 'str', 'rating': 'float32'})
+    names=['userId', 'bookId', 'rating'],
+    usecols=['userId', 'bookId', 'rating'],
+    dtype={'userId': 'int32', 'bookId': 'str', 'rating': 'float32'})
 
-# merge rating and catalog by movieID
-df = pd.merge(df_ratings,df_books,on='movieId')
+# get list of users to remove
+user_ratingCount = (df_ratings.
+     groupby(by = ['userId'])['rating'].
+     count().
+     reset_index().
+     rename(columns = {'rating': 'totalRatingCount'})
+     [['userId', 'totalRatingCount']]
+)
+users_to_remove = user_ratingCount.query('totalRatingCount >= 200').userId.tolist()
 
-# drop title
-combine_movie_rating = df.dropna(axis = 0, subset = ['title'])
+# merge rating and catalog by bookId
+df = pd.merge(df_ratings,df_books,on='bookId')
 
 # create totalRatingCount
-movie_ratingCount = (combine_movie_rating.
+book_ratingCount = (df.
      groupby(by = ['title'])['rating'].
      count().
      reset_index().
      rename(columns = {'rating': 'totalRatingCount'})
      [['title', 'totalRatingCount']]
     )
-rating_with_totalRatingCount = combine_movie_rating.merge(movie_ratingCount, left_on = 'title', right_on = 'title', how = 'left')
 
+rating_with_totalRatingCount = df.merge(book_ratingCount, left_on = 'title', right_on = 'title', how = 'left')
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
-# filter movies with more than 50 reviews
-popularity_threshold = 50
-rating_popular_movie= rating_with_totalRatingCount.query('totalRatingCount >= @popularity_threshold')
+# remove books with less than 100 ratings
+rating_popular_movie = rating_with_totalRatingCount.query('totalRatingCount >= 100')
+
+# remove from the dataset users with less than 200 ratings 
+rating_popular_movie = rating_popular_movie[rating_popular_movie['userId'].isin(users_to_remove)]
+
+# drop duplicates
+rating_popular_movie.drop_duplicates(subset=['title'])
 
 # pivot table and create matrix
-movie_features_df=rating_popular_movie.pivot_table(index='title',columns='userId',values='rating').fillna(0)
-movie_features_df_matrix = csr_matrix(movie_features_df.values)
+book_features_df = rating_popular_movie.pivot_table(index='title',columns='userId',values='rating').fillna(0)
+book_features_df_matrix = csr_matrix(book_features_df.values)
 
 # function to return recommended books - this will be tested
 def get_recommends(book = ""):
-    model_knn = NearestNeighbors(metric = 'cosine', algorithm = 'brute')
-    model_knn.fit(movie_features_df_matrix)
+    model_knn = NearestNeighbors(metric = 'cosine', n_neighbors=5, algorithm='auto')
+    model_knn.fit(book_features_df_matrix)
 
     # found book TODO: user a better search
-    for query_index in range(len(movie_features_df)):
-        if movie_features_df.index[query_index] == book:
+    for query_index in range(len(book_features_df)):
+        if book_features_df.index[query_index] == book:
             break
 
     # creating return structure
-    ret = [movie_features_df.index[query_index], []]
-    distances, indices = model_knn.kneighbors(movie_features_df.iloc[query_index,:].values.reshape(1, -1), n_neighbors = 5)
+    ret = [book_features_df.index[query_index], []]
+    distances, indices = model_knn.kneighbors(book_features_df.iloc[query_index,:].values.reshape(1, -1))
     # now we located the book. lets show the recomendations
     for i in range(1, len(distances.flatten())):
-        ret[1].append([movie_features_df.index[indices.flatten()[i]], distances.flatten()[i]])
+        ret[1].append([book_features_df.index[indices.flatten()[i]], distances.flatten()[i]])
     return ret
 
 def test_book_recommendation():
